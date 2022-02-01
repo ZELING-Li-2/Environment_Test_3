16cef78f4541dad9fe55dd0b954b798de47218b4
unknown <zli2018@tamu.edu>
2022-01-31 19:05:53 -0600

    test commit 2

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'author', with: 'jk rowling'
+    fill_in 'price', with: '5.00'
+    fill_in 'published-date', with: '2022-01-31'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('jk rowling')
+    expect(page).to have_content('5.00')
+    expect(page).to have_content('2022-01-31')
   end
 end
