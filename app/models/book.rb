c5ad07cc30cb2036393309da4e8b4cf8ad03f5a7
unknown <zli2018@tamu.edu>
2022-02-02 12:47:28 -0600

    stuff

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
     validates :title, presence: true
-    validates :test1, presence: true
+    #validates :test1, presence: true
     #validates :price, presence: true
     #validates :published-date: true
 end
